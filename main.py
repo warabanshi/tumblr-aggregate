d3effe16e5d1a7ff741f19173280d4dc1c476cd9
warabanshi <warabanshi@gmail.com>
2018-08-12 13:50:45 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import envs
+import pytumblr
+import wget
+import shutil
+import sqlite3
+import datetime, os
+
+# Authenticate via OAuth
+client = pytumblr.TumblrRestClient(
+           envs.consumer_key,
+           envs.consumer_secret,
+           envs.oauth_token,
+           envs.oauth_secret
+         )
+
+conn = sqlite3.connect(envs.sqlite_path)
+c = conn.cursor()
+
+def is_exists(reb):
+    c.execute("SELECT id FROM aggregate WHERE id = {}".format(reb["id"]))
+    if c.fetchone() == None:
+        c.execute("INSERT INTO aggregate VALUES ({}, '{}', '{}', DATETIME('now', 'localtime'))".format(
+            reb["id"], reb["short_url"], ""
+            ))
+        conn.commit()
+        return False
+    else:
+        return True
+
+def create_dir(dn):
+    if os.path.exists(dn) == False:
+        os.makedirs(dn)
+
+
+if __name__ == '__main__':
+    board = client.dashboard()
+    
+    photos = list(filter(lambda r: r["type"] == "photo", board["posts"]))
+    #print(photos[0])
+    
+    dn = envs.storage_path + '/' + str(datetime.date.today()).replace('-', '')
+    create_dir(dn)
+    
+    for p in photos:
+        if is_exists(p):
+            continue
+    
+        for r in p["photos"]:
+            url = r["original_size"]["url"]
+            if url.split(".")[-1] in ["jpg", "jpeg", "png"]:
+                print(url)
+                fn = wget.download(url)
+                shutil.move(fn, dn)
---
+++ migrate.sql
@@ -0,0 +1,6 @@
+CREATE TABLE aggregate (
+  id integer primary key,
+  short_url varchar(255),
+  image_url text,
+  created_at datetime
+);
