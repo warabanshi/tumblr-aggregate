10c39b436b79f35b17793c2c7e93d547d57fb6b8
warabanshi <warabanshi@gmail.com>
2020-03-29 17:12:00 +0900

    fixed Dockerfile and run.sh

--- migration/migrate.sh
+++ migration/migrate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-DB_PATH=/home/warabanshi/tmp/sqlite/aggregate.db;
+DB_PATH=${STORE_DIR}/sqlite/aggregate.db;
 SQL_PATH=$(dirname ${BASH_SOURCE[0]})
 
 if [ ! -e $DB_PATH ]; then
+    mkdir -p ${STORE_DIR}/sqlite
     touch $DB_PATH
     sqlite3 $DB_PATH < $SQL_PATH/migrate.sql
 fi
--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ VENV_BIN_DIR=$(poetry run which python | sed -e 's/\/python$//')
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 PYTHONPATH=${PYTHONPATH}:./tumblr_aggregate
 
+bash ${APPDIR}/migration/migrate.sh
+
 python tumblr_aggregate/app.py
